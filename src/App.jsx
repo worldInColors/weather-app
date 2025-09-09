2795044dc34930b84e97b2de21002abe6df86bec
cats <almonzer1234567@gmail.com>
2025-09-09 07:08:06 +0300

    better way of stopping weird line breaks probably

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="mx-auto flex min-h-screen max-w-[1440px] flex-col p-4 pb-12 md:p-6 md:pb-20">
       <Navbar />
-      {/* 388px breakpoint before text looks weird on mobile */}
-      <h1 className="text-preset-2 mt-12 min-w-[388px] self-center px-12 text-center md:px-30">
-        How's the sky looking today?
+      {/* Prevent weird line breaks */}
+      <h1 className="text-preset-2 mt-12 self-center px-4 text-center md:px-30">
+        How's the sky&nbsp;looking today?
       </h1>
       <SearchBar />
       <div className="justify-center lg:flex lg:gap-8 lg:px-28">
