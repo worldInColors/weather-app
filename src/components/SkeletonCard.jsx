a7222e3a08d960a3612da792c642ed6245b9cb87
cats <almonzer1234567@gmail.com>
2025-09-14 09:14:46 +0300

    Addded skeletoncard componenet

---
+++ src/components/SkeletonCard.jsx
@@ -0,0 +1,5 @@
+export default function SkeletonCard() {
+  return (
+    <div className="flex min-h-[150px] flex-1 animate-pulse flex-col rounded-xl border border-neutral-600 bg-neutral-800 px-[10px] py-4" />
+  );
+}
